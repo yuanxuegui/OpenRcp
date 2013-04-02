3b09c09d10bd563bd15bc4dff3e14bbbe8ca2ab7
yuanxuegui <yuanxuegui@163.com>
2013-04-02 21:30:38 +0800

    update

--- Build.bat
+++ Build.bat
@@ -1,5 +1,2 @@
 msbuild OpenRcp.sln /t:clean
-FOR /D %%p IN ("Build\*.*") DO rmdir "%%p" /s /q
-rmdir Build /s /q
 msbuild OpenRcp.sln /p:Configuration=Release
-#xcopy "Libs"  "Build\External" /E /Y /I /R
--- Clean.bat
+++ Clean.bat
@@ -1,3 +1,1 @@
 msbuild OpenRcp.sln /t:clean
-FOR /D %%p IN ("Build\*.*") DO rmdir "%%p" /s /q
-rmdir Build /s /q
